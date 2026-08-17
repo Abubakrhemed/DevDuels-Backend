a2571b43b2f648149734b63ba807d95f1b63b778
Abubakrhemed <abubakr.hemed@gmail.com>
2026-08-17 19:39:56 -0400

    added express middleware

--- App.ts
+++ App.ts
@@ -1,12 +1,13 @@
-import expresss from "express"
+import express from "express"
 import cors from "cors"
 import AuthROuter from "./controllers/AuthController.js"
 import leaderboardRouter from "./controllers/leaderbaordController.js"
 
-const app = expresss()
+const app = express()
 
 app.use(cors())
 app.use("/api",AuthROuter)
 app.use("/api", leaderboardRouter)
+app.use(express.json())
 
 export default app
