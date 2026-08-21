c4d4d4f6a9e3b80c1728490d359116125c753ed4
Abubakrhemed <abubakr.hemed@gmail.com>
2026-08-21 19:33:28 -0400

    code cleanup

--- App.ts
+++ App.ts
@@ -2,7 +2,6 @@ import express from "express"
 import cors from "cors"
 import AuthROuter from "./controllers/AuthController.js"
 import leaderboardRouter from "./controllers/leaderbaordController.js"
-import ApiController from "./controllers/ApiController.js"
 
 const app = express()
 
@@ -10,7 +9,6 @@ app.use(cors())
 app.use(express.json())
 app.use("/api",AuthROuter)
 app.use("/api", leaderboardRouter)
-app.use("/api", ApiController)
 app.get("/health", (req, res) => {
     res.status(200).json({ status: "ok" })
 })
